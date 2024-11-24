cba2f14bd528b06126569bc328386bcfa19569c9
KaranRaul <karanraul02@gmail.com>
2024-11-24 18:06:32 +0530

    test commit

--- python/main.py
+++ python/main.py
@@ -60,6 +60,5 @@ def detect():
         return jsonify({'disease': predicted_label})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-
 if __name__ == '__main__':
     app.run(debug=True,port=5000)
